87cb1ce4cbd8342a93f7d8ea6eee8a71da59d85b
Ravindu Perera <ravindurandika2004@gmail.com>
2025-04-18 19:36:27 +0530

    finished main banner

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -7,11 +7,13 @@ const MainBanner = () => {
     <div className='relative'>
       <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block'/>
       <img src={assets.main_banner_bg_sm} alt="banner" className='w-full  md:hidden'/>
-    <div>
-        <h1>Fresh picks daily, Deals you'll always love!</h1>
-    </div>
-    <div>
-
+    
+    <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
+        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'
+        >Fresh picks daily, Deals you'll always love!</h1>
+    
+    
+    <div className='flex items-center mt-6 font-medium'>
         <Link to={"/products"} className='group flex items-center gap-2 px-7 md:px-9 py-3 bg-[#4fbf8b] hover:bg-[#44ae7c] transition rounded text-white cursor-pointer' >
         Shop Now
       <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="arrow" />
@@ -23,6 +25,8 @@ const MainBanner = () => {
       </Link>
 
     </div>
+    </div>
+    
     </div>
   )
 }
